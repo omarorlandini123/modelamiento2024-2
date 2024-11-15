e58e55237dbf115b4da5688ceb087173902e20e2
YajairaR-ArizaG <jiminx100@gmail.com>
2024-11-15 04:17:22 -0500

    proceso de 1 pantalla

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { provideRouter, Routes, withDebugTracing } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { ApplicationConfig } from '@angular/core';
 import { BienvenidaComponent } from './bienvenida/bienvenida.component';
+import { FormularioRegistroComponent } from './formulario-registro/formulario-registro.component';
 
 export const routes: Routes = [
     { path: '', component: BienvenidaComponent },
     { path: 'login', component: LoginComponent },
+    { path: 'formulario-registro', component: FormularioRegistroComponent }
+
 ];
 
 export const appConfig: ApplicationConfig = {
--- src/app/formulario-registro/formulario-registro.component.ts
+++ src/app/formulario-registro/formulario-registro.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-formulario-registro',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatGridListModule,
+    MatTabsModule // Importa MatTabsModule aquí
+  ],
   templateUrl: './formulario-registro.component.html',
-  styleUrl: './formulario-registro.component.scss'
+  styleUrls: ['./formulario-registro.component.scss']
 })
-export class FormularioRegistroComponent {
-
-}
+export class FormularioRegistroComponent { }
