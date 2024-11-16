1078826a06b4102e91b8e50ad8d4d77da93339b8
Sebastian Romero <sebastian.romerex@gmail.com>
2024-11-16 00:33:04 -0500

    Ruta de Formulario de Envio
    Se agrega ruta "formulario-envio" y además sus respectivo componente y test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,14 +3,15 @@ import { LoginComponent } from './login/login.component';
 import { ApplicationConfig } from '@angular/core';
 import { BienvenidaComponent } from './bienvenida/bienvenida.component';
 import { FormularioRegistroComponent } from './formulario-registro/formulario-registro.component';
+import { FormularioEnvioComponent } from './formulario-envio/formulario-envio.component';
 
 export const routes: Routes = [
-    { path: '', component: BienvenidaComponent },
-    { path: 'login', component: LoginComponent },
-    { path: 'formulario-registro', component: FormularioRegistroComponent }
-
+  { path: '', component: BienvenidaComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'formulario-registro', component: FormularioRegistroComponent },
+  { path: 'formulario-envio', component: FormularioEnvioComponent },
 ];
 
 export const appConfig: ApplicationConfig = {
-    providers: [provideRouter(routes, withDebugTracing())]
-}
+  providers: [provideRouter(routes, withDebugTracing())],
+};
