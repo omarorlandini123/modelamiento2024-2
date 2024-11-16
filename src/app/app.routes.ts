60e86f096b632d6bd02c9266e5790bef1e68b6ee
sebastian2005-RP <sebastian.romerex@gmail.com>
2024-11-16 01:19:50 -0500

    Comfirmar-envio
    Aqui dejo la ruta de la confirmacion de envio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,15 @@ import { ApplicationConfig } from '@angular/core';
 import { BienvenidaComponent } from './bienvenida/bienvenida.component';
 import { FormularioRegistroComponent } from './formulario-registro/formulario-registro.component';
 import { FormularioEnvioComponent } from './formulario-envio/formulario-envio.component';
+import { ConfirmarEnvioComponent } from './confirmar-envio/confirmar-envio.component';
 
 export const routes: Routes = [
   { path: '', component: BienvenidaComponent },
   { path: 'login', component: LoginComponent },
   { path: 'formulario-registro', component: FormularioRegistroComponent },
   { path: 'formulario-envio', component: FormularioEnvioComponent },
+  { path: 'confirmar-envio', component: ConfirmarEnvioComponent },
+
 ];
 
 export const appConfig: ApplicationConfig = {
